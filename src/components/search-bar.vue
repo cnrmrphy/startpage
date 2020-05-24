64c2de47eea7175062462e173e8c6ea3d968a4d8
Conor Murphy <conor.p.murphy52@gmail.com>
2020-05-24 00:06:12 -0500

    added focus to search suggestions

--- src/components/search-bar.vue
+++ src/components/search-bar.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="search-container">
-        <input type="text" v-model="query">
+        <input type="text" v-model="query" v-on:keyup.down="increaseActive" v-on:keyup.up="decreaseActive">
         <div class="suggestions" v-if="suggestions.length > 0">
-            <p v-for="suggestion in suggestions" v-bind:key="suggestion.phrase">{{suggestion.phrase}}</p>
+            <p v-for="suggestion in suggestions" v-bind:key="suggestion.id" v-bind:class='{"active-suggestion": suggestion.id == activeSuggestion}'>{{suggestion.phrase}}</p>
         </div>
     </div>
 </template>
@@ -16,6 +16,25 @@ export default {
             acUrl: 'https://ac.duckduckgo.com/ac/',
             proxyUrl: 'https://cors-anywhere.herokuapp.com/',
             suggestions: [],
+            activeSuggestion: -1,
+        }
+    },
+    methods: {
+        decreaseActive () {
+            if(this.activeSuggestion <= 0){
+                this.activeSuggestion = this.suggestions.length - 1;
+            } else if(this.activeSuggestion > 0){
+                this.activeSuggestion --;
+            }
+        },
+        increaseActive () {
+            if(this.activeSuggestion < 0){
+                this.activeSuggestion = 0;
+            } else if (this.activeSuggestion < this.suggestions.length -1){
+                this.activeSuggestion ++;
+            } else {
+                this.activeSuggestion = 0;
+            }
         }
     },
     watch: {
@@ -26,15 +45,22 @@ export default {
             const suggUrl = this.acUrl + '?q=' + this.query.split(' ').join('+');
             const res = await fetch(this.proxyUrl + suggUrl);
             const data = await res.json();
+
+            let id = 0;
+            data.forEach(element => {
+                element.id = id;
+                id ++;
+            });
+
             this.suggestions = data;
-            console.log(res.url);
-            console.log(this.suggestions);
         }
-        
     }
 }
 </script>
 
 <style>
-
+    .active-suggestion {
+        color: blue;
+        font-size: 1.2em;
+    }
 </style>
