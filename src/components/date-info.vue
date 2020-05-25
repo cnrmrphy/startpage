fc1f9dff8288df2927f8ac2edd3e299fa83db68f
Conor Murphy <conor.p.murphy52@gmail.com>
2020-05-24 22:12:15 -0500

    clock/date is working

--- src/components/date-info.vue
+++ src/components/date-info.vue
@@ -1,14 +1,56 @@
 <template>
   <div>
-      <h1>Time</h1>
-      <h2>Date</h2>
+      <h1>{{timeString}}</h1>
+      <h2>{{dateString}}</h2>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'date-info'
-
+    name: 'date-info',
+    data() {
+        return {
+            dateOpts: {
+                weekday: 'long',
+                day: 'numeric',
+                month: 'long'
+            },
+            timeOpts: {
+               hour: 'numeric',
+               minute: 'numeric' 
+            },
+            timeString: '',
+            dateString: '',
+        }
+    },
+    methods: {
+        getDate() {
+            let clock = new Intl.DateTimeFormat('en-US', this.dateOpts).formatToParts(); 
+            let day = clock[0].value;
+            let date = clock[4].value.toString();
+            let month = clock[2].value;
+            this.dateString = day + ', ' + date + ' ' + month;
+        },
+        getTime() {
+            let clock = new Intl.DateTimeFormat('en-US', this.timeOpts).formatToParts();
+            let hours = clock[0].value.toString();
+            let minutes = clock[2].value.toString();
+            this.timeString = hours + ':' + minutes + ' ' + clock[4].value;
+            if(this.timeString == '12:00 AM'){
+                this.getDate();
+            }
+        },
+        time() {
+            this.datenow = new Date();
+        }
+    },
+    mounted() {
+        this.getDate();
+        this.interval = setInterval(this.getTime, 1000);
+    },
+    beforeDestroy() {
+        clearInterval(this.interval);
+    }
 }
 </script>
 
--- src/components/weather-info.vue
+++ src/components/weather-info.vue
@@ -1,6 +1,7 @@
 <template>
-    <div v-on:click="getLocation">
-        weather
+    <div v-on:click="getWeather">
+        <p>Weather:</p>
+        <!-- {{ weather.currently.apparentTemperature }} -->
     </div>
 </template>
 
@@ -9,22 +10,40 @@ export default {
     name: 'weather-info',
     data: function() {
         return {
-            locUrl: 'https://ipinfo.io/loc'
+            locUrl: 'https://ipinfo.io/loc',
+            weatherUrl: 'https://darksky.net/forecast/',
+            weatherParams: '/us12/en.json',
+            proxyUrl: 'https://cors-anywhere.herokuapp.com/',
+            weather: {}
         }
     },
     methods: {
         async getLocation() {
-            const request = await fetch(this.locUrl, {
+            // const request = await fetch(this.locUrl); 
+            // const data = await request.text();
+            // console.log(data);
+            // return data;
+            return ('41.8500,-87.6500');
+        },
+        async getWeather() {
+            let coords = await this.getLocation(); 
+            let requestUrl = this.proxyUrl + this.weatherUrl + coords + this.weatherParams;
+
+            const request = await fetch(requestUrl, {
                 headers: {
-                    'accept': 'application/json',
-                    'User-Agent': 'startpage-tab/1.0',
-                    'origin': 'startpage.cmurph.me'
+                    "Origin": "startpage.cmurph.me"
                 }
-            }); 
+            });
             const data = await request.json();
+            this.weather = data;
             console.log(data);
+            setTimeout(this.getWeather(), 60000);
         }
-    }
+    },
+    // beforeCreated() {
+        // this.getWeather();
+        // this.interval = setInterval(() => this.getWeather(), 60000);
+    // }
 }
 </script>
 
